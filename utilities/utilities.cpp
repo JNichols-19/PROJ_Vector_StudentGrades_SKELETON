4a90e272db59e0f241288e8126bccb868d3bcf22
JNichols-19 <julia.nichols.19@cnu.edu>
2021-10-01 13:25:48 -0400

    added functions from utils.h to utils.cpp and set the return to 0 as a place holder

--- utilities/utilities.cpp
+++ utilities/utilities.cpp
@@ -1,7 +1,7 @@
 //============================================================================
 // Name        : utilities.cpp
 // Author      : Julia Nichols
-// Version     : 9/30/21
+// Version     : 10/1/21
 // Copyright   : Your copyright notice
 //============================================================================
 
@@ -42,3 +42,57 @@ double stringToDouble(const char *myString) {
 	return atof(myString);
 }
 
+/***
+ * Clears allStudentData, Opens file, For each line; parses name, midterm1,midterm2 and an 
+ * optional finalgrade (if present in file)from that line. Puts this info into a studentData struct,
+ * adds this struct to allstudentData vector.
+ * NOTE:
+ * Ignore empty lines
+ * Non empty lines will contain 4 or 5 values to be parsed.
+ * the default param separator_char is defined in the header file declaration of readFile ONLY, it is not in the readFile definition.
+ * @param file - where student data is stored
+ * @param allstudentData - a vector that holds the student data info after it has been read from file
+ * @param separator_char - the character that separates each bit of data in a line of student data
+ * @return COULD_NOT_OPEN_FILE
+ *         SUCCESS
+ */
+int readFile(std::string &file, std::vector<KP::studentData> &allstudentData, char separator_char=KP::SEPERATOR_CHAR ) {
+	return int(0);
+}
+
+/***
+ * Iterates over all students in vector, and averages midterm1 and midterm2 to get the finalgrade
+ * @param allstudentData
+ * @return VECTOR_CONTAINS_NO_STUDENTS
+ *         SUCCESS
+ */
+int calculateFinalGrade(std::vector<KP::studentData> &allstudentData) {
+	return int(0);
+}
+//void extractFailingStudents(double failgrade = FAILGRADE);
+
+/***
+ * Opens file, serializes data from allstudentData to file, closes file 
+ * the default param seperator_char is defined in the header file ONLY
+ * @param file - where student data is stored
+ * @param allstudentData - a vector that holds student data info that will be written to file
+ * @param separator_char - the character that separates each bit of data in a line of student data
+ * @return VECTOR_CONTAINS_NO_STUDENTS
+ *         COULD_NOT_OPEN_FILE
+ *         SUCCESS
+ */
+int writeFile(std::string &file, std::vector<KP::studentData> &allstudentData, char separator_char=KP::SEPERATOR_CHAR ) {
+	return int(0);
+}
+
+//sorts studentdata based on SORT_TYPE
+/***
+ * 
+ * @param allstudentData - a vector that holds student data info that will be written to file
+ * @param st - how to sort the vector
+ * @return VECTOR_CONTAINS_NO_STUDENTS
+ *         SUCCESS
+ */
+int sortStudentData(std::vector<KP::studentData> &allstudentData,KP::SORT_TYPE st) {
+	return int(0);
+}
